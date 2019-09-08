155cf53e79d6905292e98c6f86b86d7c1e54202c
patrik-setterberg <setterberg.patrik@gmail.com>
2019-09-08 17:37:32 +0200

    Add gulp, postcss
    Plugins: autoprefixer, nesting
    Also install module responsive images

---
+++ web/themes/custom/blogtheme/gulpfile.js
@@ -0,0 +1,29 @@
+let gulp = require('gulp');
+let postcss = require('gulp-postcss');
+let autoprefixer = require('autoprefixer');
+let nesting = require('postcss-nesting');
+
+
+gulp.task('css', function () {
+
+    let plugins = [
+        nesting,
+        autoprefixer()
+    ];
+
+    return gulp.src('src/css/*.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/css/*.css', gulp.series('css'));
+});
+
+function defaultTask(cb) {
+
+    // code
+    cb();
+}
+exports.default = defaultTask
+
