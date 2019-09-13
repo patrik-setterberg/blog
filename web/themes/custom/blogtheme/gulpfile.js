1078f4edda4036a8d84e9deba12462044f462569
patrik-setterberg <setterberg.patrik@gmail.com>
2019-09-13 23:29:54 +0200

    Set up media queries
    Install plugins postcss-import, postcss-css-variables,
    postcss-custom-media
    Disable CSS aggregation

--- web/themes/custom/blogtheme/gulpfile.js
+++ web/themes/custom/blogtheme/gulpfile.js
@@ -1,14 +1,22 @@
 let gulp = require('gulp');
 let postcss = require('gulp-postcss');
+let customMedia = require('postcss-custom-media');
+let postcssImport = require('postcss-import');
 let autoprefixer = require('autoprefixer');
 let nesting = require('postcss-nesting');
+let variables = require('postcss-css-variables');
 
 
 gulp.task('css', function () {
 
     let plugins = [
+        postcssImport({ root: '/src/css/' }),
         nesting,
-        autoprefixer()
+        customMedia({
+            importFrom: '/home/spacedoc/projects/blog/web/themes/custom/blogtheme/src/css/variables.css',
+        }),
+        autoprefixer({grid: true}),
+        variables()
     ];
 
     return gulp.src('src/css/*.css')
